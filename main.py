374e0d1902e94e685ccbabdab1a2dd58a18c28c3
TerryonZhao <tinkyzzz0704@gmail.com>
2025-08-04 21:33:19 +0800

    Update for Action

--- main.py
+++ main.py
@@ -4,28 +4,21 @@ from src.send_email import send_rain_email
 import datetime
 
 def main():
-    log_path = "/private/tmp/daily.log"
-    with open(log_path, 'a') as f:
-        f.write(f"Script triggered at {datetime.datetime.now()}\n")
-
-        weather_data = fetch()
-        if weather_data:
-            try:
-                rain_summary = rain_process(weather_data)
-                print("Step 3: Send rain email...")
-                if send_rain_email(rain_summary):
-                    f.write("Email sent successfully.\n")
-                else:
-                    f.write("No rain expected today.\n")
-            except Exception as e:
-                print(f"Error processing rain data: {e}")
-                f.write(f"Error: {e}\n")
-        else:
-            print("No weather data available.")
-            f.write("No weather data available.\n")
-
-        f.write("<<<<<<<<<<<< New Log >>>>>>>>>>>>\n")
-
+    print(f"Script triggered at {datetime.datetime.now()}\n")
+    weather_data = fetch()
+    if weather_data:
+        try:
+            rain_summary = rain_process(weather_data)
+            print("Step 3: Send rain email...")
+            if send_rain_email(rain_summary):
+                print("Email sent successfully.\n")
+            else:
+                print("No rain expected today.\n")
+        except Exception as e:
+            print(f"Error processing rain data: {e}")
+    else:
+        print("No weather data available.\n")
+    print("<<<<<<<<<<<< New Log >>>>>>>>>>>>\n")
 
 
 if __name__ == "__main__":
--- src/fetch_weather.py
+++ src/fetch_weather.py
@@ -14,7 +14,7 @@ def load_config():
     try:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(current_dir, '..', 'config.json')
-        
+
         with open(config_path, 'r', encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
